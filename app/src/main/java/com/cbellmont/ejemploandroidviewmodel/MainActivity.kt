9bab86f960ebee3ad9c031407b6c3aa19c3ebf4e
Celeste <cecuercu@gmail.com>
2021-01-11 18:30:02 +0100

    Primer Ejemplo

--- app/src/main/java/com/cbellmont/ejemploandroidviewmodel/MainActivity.kt
+++ app/src/main/java/com/cbellmont/ejemploandroidviewmodel/MainActivity.kt
@@ -1,51 +1,108 @@
 package com.cbellmont.ejemploandroidviewmodel
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.cbellmont.ejemploandroidviewmodel.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var model :MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val model = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        model = ViewModelProvider(this).get(MainActivityViewModel::class.java)
 
-        model.downloadFilms()
-
-        model.films.forEach {
-            binding.tvFilms.append("${it.name}\n")
-        }
+        downloadAll()
 
         binding.button1.setOnClickListener {
             binding.tvFilms.text = ""
+            binding.pbLoading.visibility = View.VISIBLE
 
-            model.getBoton1().forEach {
-                binding.tvFilms.append("${it.name}\n")
-            }
+            downloadBoton1()
         }
 
         binding.button2.setOnClickListener {
             binding.tvFilms.text = ""
+            binding.pbLoading.visibility = View.VISIBLE
 
-            model.getBoton2().forEach {
-                binding.tvFilms.append("${it.name}\n")
+            CoroutineScope(IO).launch {
+                val list = model.getBoton2()
+
+                withContext (Main) {
+                    model.getBoton2().forEach {
+                        binding.tvFilms.append("${it.name}\n")
+                    }
+                    binding.pbLoading.visibility = View.GONE
+                }
             }
         }
 
         binding.button3.setOnClickListener {
             binding.tvFilms.text = ""
+            binding.pbLoading.visibility = View.VISIBLE
 
+            downloadBoton3()
+        }
+    }
+
+    private fun downloadAll(){
+        CoroutineScope(IO).launch {
+            val list = model.getFilms()
+            setTextOnMainThread(list)
+        }
+    }
+
+    private fun downloadBoton1(){
+        CoroutineScope(IO).launch {
+            val list = model.getBoton1()
+            setTextBoton1(list)
+        }
+    }
+
+    private fun downloadBoton3(){
+        CoroutineScope(IO).launch {
+            val list = model.getBoton3()
+            setTextBoton3(list)
+        }
+    }
+
+    private suspend fun setTextOnMainThread(list: MutableList<Film>) {
+        withContext (Main) {
+            list.forEach {
+                binding.tvFilms.append("${it.name}\n")
+            }
+            binding.pbLoading.visibility = View.GONE
+        }
+    }
+
+    private suspend fun setTextBoton1(list: MutableList<Film>) {
+        withContext (Main) {
+            model.getBoton1().forEach {
+                binding.tvFilms.append("${it.name}\n")
+            }
+            binding.pbLoading.visibility = View.GONE
+        }
+    }
+
+    private suspend fun setTextBoton3(list: MutableList<Film>) {
+        withContext (Main) {
             model.getBoton3().forEach {
                 binding.tvFilms.append("${it.name}\n")
             }
+            binding.pbLoading.visibility = View.GONE
         }
-
     }
 }
--- app/src/main/java/com/cbellmont/ejemploandroidviewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/cbellmont/ejemploandroidviewmodel/MainActivityViewModel.kt
@@ -1,36 +1,63 @@
 package com.cbellmont.ejemploandroidviewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class MainActivityViewModel  : ViewModel() {
 
-    val films = mutableListOf<Film>()
+    private val films = mutableListOf<Film>()
 
-    fun downloadFilms() {
-        films.addAll(mutableListOf(
-            Film(1, "La Amenaza Fantasma", "aaaa"),
-            Film(2, "El Ataque de los Clones", "aaaa"),
-            Film(3, "La Venganza de los Sith", "aaaa"),
-            Film(4, "Una Nueva Esperanza", "aaaa"),
-            Film(5, "El Imperio Contraataca", "aaaa"),
-            Film(6, "El Retorno del Jedi", "aaaa"),
-            Film(4, "El Despertar de la Fuerza", "aaaa"),
-            Film(5, "Los Útimos Jedi", "aaaa"),
-            Film(6, "El Ascenso de Skywalker", "aaaa")
-        ))
+    suspend fun getFilms(): MutableList<Film> {
+        return withContext(Dispatchers.IO) {
+            delay(5000)
+            if (films.isEmpty()) {
+                downloadFilms()
+            }
+            return@withContext films
+        }
     }
 
-    fun getBoton1() : List<Film> {
-      return films.subList(0,3)
+    private fun downloadFilms() {
+        viewModelScope.launch {
+            films.addAll(
+                mutableListOf(
+                    Film(1, "La Amenaza Fantasma", "aaaa"),
+                    Film(2, "El Ataque de los Clones", "aaaa"),
+                    Film(3, "La Venganza de los Sith", "aaaa"),
+                    Film(4, "Una Nueva Esperanza", "aaaa"),
+                    Film(5, "El Imperio Contraataca", "aaaa"),
+                    Film(6, "El Retorno del Jedi", "aaaa"),
+                    Film(4, "El Despertar de la Fuerza", "aaaa"),
+                    Film(5, "Los Útimos Jedi", "aaaa"),
+                    Film(6, "El Ascenso de Skywalker", "aaaa")
+                )
+            )
+        }
     }
 
-    fun getBoton2() : List<Film> {
-        return films.subList(3,6)
+    suspend fun getBoton1() : MutableList<Film> {
+        return withContext(Dispatchers.IO) {
+            delay(2000)
+            return@withContext films.subList(0,3)
+        }
     }
 
-    fun getBoton3() : List<Film> {
-        return films.subList(6,9)
+    suspend fun getBoton2() : MutableList<Film> {
+        return withContext(Dispatchers.IO) {
+            delay(2000)
+            return@withContext films.subList(3,6)
+        }
     }
 
+    suspend fun getBoton3() : MutableList<Film> {
+        return withContext(Dispatchers.IO) {
+            delay(2000)
+            return@withContext films.subList(6,9)
+        }
+    }
 }
